ee87eab821a9338840712d666b8413c19f2b87db
atat-4311 <atatsecondaryemail@proton.me>
2025-02-06 19:47:50 -0500

    Commented the Code

--- src/main/java/frc/robot/Lights.java
+++ src/main/java/frc/robot/Lights.java
@@ -19,3 +19,7 @@ public class Lights extends RobotContainer {
     }
 
 }
+
+/*it feels wrong, but the way that they made the blinkin is that the PWM range is the same as a PWM Sparkmax,
+ so you can control it the same as a sparkmax. Link to the conversion chart: 
+ https://1166281274-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F-ME3KPEhFI6-MDoP9nZD%2Fuploads%2FMOYJvZmWgxCVKJhcV5fn%2FREV-11-1105-LED-Patterns.pdf?alt=media&token=e8227890-6dd3-498d-834a-752fa43413fe */
