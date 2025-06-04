4a23c8570863b2051f5f1d73d5a2ad3df949c209
atat-4311 <atatsecondaryemail@proton.me>
2025-06-04 17:31:57 -0400

    Update DeAlgae.java

--- src/main/java/frc/robot/commands/DeAlgae.java
+++ src/main/java/frc/robot/commands/DeAlgae.java
@@ -1,64 +1,64 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
-// public class DeAlgae extends Command {
+public class DeAlgae extends Command {
 
-//   int timer;
-//   public static double position;
-//   public static boolean raising;
-//   private final Intake m_intake;
-//   private final double finalPos;
+  int timer;
+  public static double position;
+  public static boolean raising;
+  private final Intake m_intake;
+  private final double finalPos;
 
-//   // Called when the command is initially scheduled.
+  // Called when the command is initially scheduled.
 
-//   public DeAlgae(Intake intake, double maxPos) {
-//     m_intake = intake;
-//     finalPos = maxPos;
-//     addRequirements(m_intake);
-//   }
+  public DeAlgae(Intake intake, double maxPos) {
+    m_intake = intake;
+    finalPos = maxPos;
+    addRequirements(m_intake);
+  }
 
-//   @Override
-//   public void initialize() {
+  @Override
+  public void initialize() {
 
-//     if (m_intake.getDeAlgaeUp()) {
-//       m_intake.deAlgae_mDown();
-//       raising = false;
-//       m_intake.setDeAlgaeUp(false);
-//     } else {
-//       m_intake.deAlgae_mUp();
-//       raising = true;
-//       m_intake.setDeAlgaeUp(true);
-//     }
+    if (m_intake.getDeAlgaeUp()) {
+      m_intake.deAlgae_mDown();
+      raising = false;
+      m_intake.setDeAlgaeUp(false);
+    } else {
+      m_intake.deAlgae_mUp();
+      raising = true;
+      m_intake.setDeAlgaeUp(true);
+    }
 
-//     // if m_intake.
+    // if m_intake.
 
-//     // timer = 0;
-//   }
+    // timer = 0;
+  }
 
-//   // Called every time the scheduler runs while the command is scheduled.
-//   @Override
-//   public void execute() {
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
 
-//     timer++;
-//   }
+    timer++;
+  }
 
-//   // Called once the command ends or is interrupted.
-//   @Override
-//   public void end(boolean interrupted) {
-//     m_intake.stopDeAlgae_m();
-//   }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_intake.stopDeAlgae_m();
+  }
 
-//   // Returns true when the command should end.
-//   @Override
-//   public boolean isFinished() {
-//     if (raising && m_intake.getDeAlgaePos() > finalPos) {
-//       return true;
-//     } else if (!raising && m_intake.getDeAlgaePos() <= 0) {
-//       return true;
-//     }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (raising && m_intake.getDeAlgaePos() > finalPos) {
+      return true;
+    } else if (!raising && m_intake.getDeAlgaePos() <= 0) {
+      return true;
+    }
 
-//     return false;
-//   }
-// }
+    return false;
+  }
+}
